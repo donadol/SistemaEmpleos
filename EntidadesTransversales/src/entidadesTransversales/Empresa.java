3027d946eeb5858cf4ce9d19799b260389622ee1
Laura Donado <ldonado11@gmail.com>
2019-11-10 12:40:55 -0500

    cambios en atributos de oferta y empresa

--- EntidadesTransversales/src/entidadesTransversales/Empresa.java
+++ EntidadesTransversales/src/entidadesTransversales/Empresa.java
@@ -3,6 +3,23 @@ package entidadesTransversales;
 import java.util.List;
 
 public class Empresa {
+	public String nombre;
 	public List<Oferta> ofertas;
-
+	public Empresa(String nombre, List<Oferta> ofertas) {
+		super();
+		this.nombre = nombre;
+		this.ofertas = ofertas;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+	public void setOfertas(List<Oferta> ofertas) {
+		this.ofertas = ofertas;
+	}
 }
--- EntidadesTransversales/src/entidadesTransversales/Oferta.java
+++ EntidadesTransversales/src/entidadesTransversales/Oferta.java
@@ -2,18 +2,30 @@ package entidadesTransversales;
 
 
 public class Oferta {
+	public int id;
 	public String cargo;
 	public long salario;
+	public int experiencia;
 	public NivelEstudios nivelEstudios;
 	public SectorEmpresa sectorEmpresa;
 	public Cita[] citas;
-	public Oferta(String cargo, long salario, NivelEstudios nivelEstudios, SectorEmpresa sectorEmpresa) {
+	
+	public Oferta(int id, String cargo, long salario, int experiencia, NivelEstudios nivelEstudios,
+			SectorEmpresa sectorEmpresa) {
 		super();
+		this.id = id;
 		this.cargo = cargo;
 		this.salario = salario;
+		this.experiencia = experiencia;
 		this.nivelEstudios = nivelEstudios;
 		this.sectorEmpresa = sectorEmpresa;
 		this.citas = new Cita[3];
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getCargo() {
 		return cargo;
@@ -26,6 +38,12 @@ public class Oferta {
 	}
 	public void setSalario(long salario) {
 		this.salario = salario;
+	}
+	public int getExperiencia() {
+		return experiencia;
+	}
+	public void setExperiencia(int experiencia) {
+		this.experiencia = experiencia;
 	}
 	public NivelEstudios getNivelEstudios() {
 		return nivelEstudios;
