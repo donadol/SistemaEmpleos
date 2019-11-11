ca3c3ffe0574838d3a0d0d2e2ca2b576c3649152
Laura Donado <ldonado11@gmail.com>
2019-11-11 11:36:46 -0500

    empresa check

--- Empresas/src/servicios/EmpresaMain.java
+++ Empresas/src/servicios/EmpresaMain.java
@@ -1,20 +1,40 @@
 package servicios;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
+import GUI.FrameEmpresa;
 import entidadesTransversales.Empresa;
+import interfaces.ISolicitudOferta;
 import utils.Utils;
 
 public class EmpresaMain {
-
+	static Empresa empresa;
+	static FrameEmpresa frameEmpresa;
+	static ISolicitudOferta interfaz;
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) throws InterruptedException {
+		try {
+			Registry registry = LocateRegistry.getRegistry();
+			interfaz = (ISolicitudOferta)registry.lookup("SolOferta");
+		}catch(Exception e) {
+			System.err.println("Empresa exception: " + e.toString());
+			e.printStackTrace();
+		}
 		List<Empresa> empresas = Utils.cargarEmpresas("./empresas.json");
-		for(Empresa e: empresas) {
-			EmpresaThread empresaThread = new EmpresaThread(e);
-			empresaThread.run();
-			TimeUnit.SECONDS.sleep(10);
+		int i = sc.nextInt();
+		empresa = empresas.get(i);
+		try {
+			frameEmpresa = new FrameEmpresa(empresa);
+			frameEmpresa.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for(int i1=0; i1<empresa.getOfertas().size();++i1) {
+			OfertaThread ofertaThread = new OfertaThread(i1, frameEmpresa, interfaz, empresa.getOfertas().get(i));
+			ofertaThread.run();
 		}
 	}
-
 }
---
+++ Empresas/src/servicios/OfertaThread.java
@@ -0,0 +1,38 @@
+package servicios;
+
+import java.rmi.RemoteException;
+
+import GUI.FrameEmpresa;
+import entidadesTransversales.Candidato;
+import entidadesTransversales.NotiOferta;
+import entidadesTransversales.Oferta;
+import interfaces.ISolicitudOferta;
+
+public class OfertaThread implements Runnable{
+	private int id;
+	private FrameEmpresa frame;
+	private ISolicitudOferta interfaz;
+	private Oferta oferta;
+
+	public OfertaThread(int id, FrameEmpresa frame, ISolicitudOferta interfaz, Oferta oferta) {
+		super();
+		this.id = id;
+		this.frame = frame;
+		this.interfaz = interfaz;
+		this.oferta = oferta;
+	}
+
+	@Override
+	public void run() {
+		try {
+			NotiOferta response = interfaz.subscribirOferta(oferta);
+			for(Candidato c: response.getCandidatos()) {
+				frame.actualizarTable(c, id);
+			}
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
