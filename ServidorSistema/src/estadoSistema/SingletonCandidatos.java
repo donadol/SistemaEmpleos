432f8ec53b259322d3815d332933dc2b374ada7f
Pedro Santiago Lizarazo <p-lizarazo@javeriana.edu.co>
2019-11-11 15:09:41 -0500

    Update SingletonCandidatos.java

--- ServidorSistema/src/estadoSistema/SingletonCandidatos.java
+++ ServidorSistema/src/estadoSistema/SingletonCandidatos.java
@@ -1,5 +1,34 @@
 package estadoSistema;
 
-public class SingletonCandidatos {
+import java.util.ArrayList;
 
+import entidadesTransversales.Candidato;
+
+public class SingletonCandidatos {
+	
+	private static SingletonCandidatos instance  = new SingletonCandidatos();
+	
+	private static ArrayList<Candidato> listaCandidatos;
+	
+	
+	private SingletonCandidatos() {
+		listaCandidatos = new ArrayList<>();
+	};
+	
+	public static SingletonCandidatos getInstance() {
+		return instance;
+	}
+
+	public static void setInstance(SingletonCandidatos instance) {
+		SingletonCandidatos.instance = instance;
+	}
+
+	public static ArrayList<Candidato> getListaCandidatos() {
+		return listaCandidatos;
+	}
+
+	public static void setListaCandidatos(ArrayList<Candidato> listaCandidatos) {
+		SingletonCandidatos.listaCandidatos = listaCandidatos;
+	}
+	
 }
